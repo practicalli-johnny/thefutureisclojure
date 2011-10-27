a0bf23a54ffc9c1408b47335360a2386cee5774f
Bodil Stokke <bodil@bodil.tv>
2011-10-27 04:55:06 +0200

    Separate namespaces for each WebSocket connection.

--- src/server.clj
+++ src/server.clj
@@ -6,22 +6,31 @@
            [org.webbitserver.handler StaticFileHandler]))
 
 (defn on-message [socket json]
-  (let [msg (str "(ns repl) " (json/read-json json))]
+  (let [repl-ns (.data socket "clj-namespace")
+        msg (str "(ns " repl-ns ")\n" (json/read-json json))]
     (try
       (let [result (load-string msg)]
-        (println result)
         (.send socket (json/json-str
-                       {:result (with-out-str (pprint/pprint result))})))
+                       {:result (with-out-str (pprint/pprint result))
+                        :ns repl-ns})))
       (catch Exception e
         (.send socket (json/json-str
-                       {:error (.getMessage (main/repl-exception e))}))))))
+                       {:error (.getMessage (main/repl-exception e))
+                        :ns repl-ns}))))))
 
 (defn start [port]
   (doto (WebServers/createWebServer port)
     (.add "/repl"
           (proxy [WebSocketHandler] []
-            (onOpen [c] (println "opened" c))
-            (onClose [c] (println "closed" c))
+            (onOpen [c]
+              (let [repl-ns (str "repl.user" (.hashCode c))]
+                (create-ns (symbol repl-ns))
+                (.data c "clj-namespace" repl-ns)
+                (println "opened" repl-ns)))
+            (onClose [c]
+              (let [repl-ns (.data c "clj-namespace")]
+                (remove-ns (symbol repl-ns))
+                (println "closed" repl-ns)))
             (onMessage [c j] (on-message c j))))
     (.add (StaticFileHandler. "resources/public"))
     (.start)))
